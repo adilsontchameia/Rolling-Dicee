8eb1ae57f84499d8e068b6848902af023a5e1f4c
adilsonchameia <adilsonkchameia@gmail.com>
2021-04-05 18:49:35 +0100

    redimensionada as imagens

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,23 @@ class _HomeState extends State<Home> {
         title: Text("Dicee"),
         backgroundColor: Colors.red,
       ),
-      body: Row(
-        children: [
-          Expanded(
-            child: Image.asset("dice1.png"),
-          ),
-          Expanded(
-            child: Image.asset("dice1.png"),
-          ),
-        ],
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("dice1.png"),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("dice1.png"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
